e851411dcea2d0d7642119fff8251d5ff307af57
chilejay7 <codyburk7@yahoo.com>
2023-11-07 18:25:17 -0700

    created schema and seeds

---
+++ db/schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INT AUTO_INCREMENT=500 PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT=1000 PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,25 +1,21 @@
-CREATE TABLE department (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL
-);
+INSERT INTO department (name)
+VALUES
+    ('IT'),
+    ('Informatics'),
+    ('Finance');
 
-CREATE TABLE role (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30),
-    salary DECIMAL,
-    department_id INT
-    FOREIGN KEY (department_id)
-    REFERENCES department(id)
-    ON DELETE SET NULL
-);
+INSERT INTO role (title, salary, department_id)
+    ('Junior Accountant', 60000, 0003),500
+    ('Informaticist I', 65000, 0002), 501
+    ('Technical Analyst', 75000, 0002), 502
+    ('Manager', 100000, 0003), 503
+    ('Director', 200000, 0001), 504
+    ('CEO', 500000, 0001); 505
 
-CREATE TABLE employee (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
-    manager_id INT
-    FOREIGN KEY (role_id)
-    REFERENCES role(id)
-);
-
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+    ('Tim', 'Smith', 500, 1003),
+    ('John', 'Smith', 501, 1003)
+    ('Eric', 'Moody', 502, 1003)
+    ('Eric', 'Moody', 503, 1004)
+    ('Alli', 'Burkholder', 504, 1005),
+    ('Katelyn', 'Schumacher', 505);
