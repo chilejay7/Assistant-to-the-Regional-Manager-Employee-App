9e2dcdb16c22e9cb59d6ac0e2875cb3f128754d7
chilejay7 <codyburk7@yahoo.com>
2023-11-07 21:10:03 -0700

    creates test db.promise query

--- server.js
+++ server.js
@@ -5,11 +5,20 @@ const db = mysql.createConnection (
     {
         host: 'localhost',
         user: 'root',
-        password: '',
+        password: 'pass',
         database: 'employees_db'
     }
 );
 
-db.query('SELECT * FROM employee', (err, results) => {
-    console.log(results)
+db.promise().query('SELECT * FROM employee')
+.then(([rows, fields]) => {
+    console.log(rows);
+    return rows;
+})
+.then((rows) => {
+    rows.forEach((r) => {
+        const { id, first_name, last_name, role_id, manager_id } = r;
+        console.log(first_name);
+    })
+    console.log(rows[3]);
 });
